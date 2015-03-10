dc7737ddde9fa5fddd1760c93a9388c856993122
Zachary Whaley <zachbwhaley@gmail.com>
2015-03-10 14:03:13 -0500

    Add travis install script
    Downloads, builds, and installs a hunspell 1.3.3 debian package with custom dictionaries

---
+++ script/install-hunspell.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo echo "DGET_VERIFY=no" > ~/.devscripts
+sudo apt-get install dpkg-dev devscripts dh-autoreconf
+sudo dget -x --build http://http.debian.net/debian/pool/main/h/hunspell/hunspell_1.3.3-3.dsc
+sudo dpkg -i -R .
+
+EN_US_DICT="en_US-zwhaley.tar.gz"
+curl -v -L -o $EN_US_DICT https://dl.dropboxusercontent.com/u/55259404/hunspell-en_US-zachwhaley.tar.gz
+sudo mkdir -v -p /usr/share/hunspell
+sudo tar -v -C /usr/share/hunspell -xzf $EN_US_DICT
