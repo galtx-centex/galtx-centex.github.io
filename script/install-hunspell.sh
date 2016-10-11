8116694209d14788c9c7b27e2eff3346ca22add6
Zachary Whaley <zachbwhaley@gmail.com>
2016-10-11 16:19:46 -0500

    Use Docker CI

--- script/install-hunspell.sh
+++ script/install-hunspell.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 sudo echo "DGET_VERIFY=no" > ~/.devscripts
-sudo apt-get install -y dpkg-dev devscripts dh-autoreconf
 sudo dget -x --build http://http.debian.net/debian/pool/main/h/hunspell/hunspell_1.3.3-3.dsc
 sudo dpkg -i -R .
 
